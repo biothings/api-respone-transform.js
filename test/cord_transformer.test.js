d1f77ce182a18c77b672ec297c7e67fc1cc1d278
jiwen xin <kevinxin@scripps.edu>
2020-07-13 14:49:36 -0700

    separate cord and semmed test from main test

---
+++ test/cord_transformer.test.js
@@ -0,0 +1,117 @@
+const cord_tf = require("../transformers/cord_transformer");
+const axios = require("axios");
+
+describe("test cord transformer", () => {
+
+    let api_response, input;
+
+    beforeAll(async () => {
+        let res = await axios({
+            method: 'post',
+            url: 'https://biothings.ncats.io/cord_gene/query',
+            data: 'q=238, 239, 240&scopes=hgnc',
+            params: {
+                fields: 'associated_with'
+            }
+        })
+        api_response = res.data;
+        input = {
+            response: api_response,
+            edge: {
+                "input": ["238", "239", "240"],
+                "query_operation": {
+                    "params": {
+                        "fields": "associated_with"
+                    },
+                    "request_body": {
+                        "body": {
+                            "q": "{inputs[0]}",
+                            "scopes": "hgnc"
+                        },
+                        "header": "application/x-www-form-urlencoded"
+                    },
+                    "path": "/query",
+                    "path_params": [],
+                    "method": "post",
+                    "server": "https://biothings.ncats.io/cord_gene",
+                    "tags": [
+                        "gene",
+                        "annotation",
+                        "query",
+                        "translator",
+                        "biothings"
+                    ],
+                    "supportBatch": true,
+                    "inputSeparator": ","
+                },
+                "association": {
+                    "input_id": "HGNC",
+                    "input_type": "Gene",
+                    "output_id": "HGNC",
+                    "output_type": "Gene",
+                    "predicate": "related_to",
+                    "source": "Translator Text Mining Provider",
+                    "api_name": "CORD Gene API",
+                    "smartapi": {
+                        "id": "6bc54230a6fa7693b2cd113430387ca7",
+                        "meta": {
+                            "ETag": "5e7512d15d24b57b52cb15604aaa6c24192f48ef00da9732f23aab3707b2061b",
+                            "github_username": "kevinxin90",
+                            "timestamp": "2020-04-29T00:00:40.725359",
+                            "uptime_status": "good",
+                            "uptime_ts": "2020-06-12T00:05:25.251375",
+                            "url": "https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/master/cord/cord_gene.yml"
+                        }
+                    }
+                },
+                "response_mapping": {
+                    "related_to": {
+                        "HGNC": "associated_with.hgnc",
+                        "pmc": "associated_with.pmc"
+                    }
+                },
+                "id": "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b"
+            }
+        }
+    });
+
+    test("test cord pairInputWithAPIResponse", () => {
+        let tf = new cord_tf(input);
+        let res = tf.pairInputWithAPIResponse();
+        expect(res).toHaveProperty('HGNC:240');
+        expect(res["HGNC:239"]).toBeUndefined();
+    });
+
+    test("test wrapper", () => {
+        let tf = new cord_tf(input);
+        let res = tf.wrap(input.response[0]);
+        expect(res).toHaveProperty("related_to");
+        expect(res.related_to[0]['@type']).toBe("Gene");
+    });
+
+    test("test json transform", () => {
+        let tf = new cord_tf(input);
+        let res = tf.jsonTransform(input.response[0]);
+        expect(res).toEqual(input.response[0]);
+    });
+
+    test("add edge info", () => {
+        let tf = new cord_tf(input);
+        let res = tf.pairInputWithAPIResponse();
+        let rec = res["HGNC:238"][0];
+        rec = tf.wrap(rec);
+        let result = tf.addEdgeInfo("HGNC:238", rec["related_to"][0]);
+        expect(result[0]).toHaveProperty("$association");
+        expect(result[0].$association.api_name).toBe("CORD Gene API")
+    });
+
+    test("test main function transform", () => {
+        let tf = new cord_tf(input);
+        let res = tf.transform();
+        expect(res[0]).toHaveProperty('HGNC');
+        expect(res[0]).toHaveProperty("$association");
+        expect(res[0]).toHaveProperty("$input", "HGNC:238");
+        expect(res.slice(-1)[0]).toHaveProperty("$input", "HGNC:240");
+        expect(res.length).toBeGreaterThan(30);
+    })
+})
---
+++ test/semmed_transformer.test.js
@@ -0,0 +1,119 @@
+const semmed_tf = require("../transformers/semmed_transformer");
+const axios = require("axios");
+
+describe("test semmed transformer", () => {
+
+    let api_response, input;
+
+    beforeEach(async () => {
+        let res = await axios({
+            method: 'post',
+            url: 'https://biothings.ncats.io/semmedgene/query',
+            data: 'q=C1332823, C1332824, 123&scopes=umls',
+            params: {
+                fields: 'name,umls,positively_regulates',
+                size: '5'
+            }
+        })
+        api_response = res.data;
+        input = {
+            response: api_response,
+            edge: {
+                "input": ["C1332824", "C1332823", "123"],
+                "query_operation": {
+                    "params": {
+                        "fields": "positively_regulates"
+                    },
+                    "request_body": {
+                        "body": {
+                            "q": "{inputs[0]}",
+                            "scopes": "umls"
+                        }
+                    },
+                    "path": "/query",
+                    "path_params": [],
+                    "method": "post",
+                    "server": "https://biothings.ncats.io/semmedgene",
+                    "tags": [
+                        "disease",
+                        "annotation",
+                        "query",
+                        "translator",
+                        "biothings",
+                        "semmed"
+                    ],
+                    "supportBatch": true,
+                    "inputSeparator": ","
+                },
+                "association": {
+                    "input_id": "UMLS",
+                    "input_type": "Gene",
+                    "output_id": "UMLS",
+                    "output_type": "Gene",
+                    "predicate": "positively_regulates",
+                    "source": "SEMMED",
+                    "api_name": "SEMMED Gene API",
+                    "smartapi": {
+                        "id": "81955d376a10505c1c69cd06dbda3047",
+                        "meta": {
+                            "ETag": "f94053bc78b3c2f0b97f7afd52d7de2fe083b655e56a53090ad73e12be83673b",
+                            "github_username": "kevinxin90",
+                            "timestamp": "2020-05-27T16:53:40.804575",
+                            "uptime_status": "good",
+                            "uptime_ts": "2020-06-12T00:04:31.404599",
+                            "url": "https://raw.githubusercontent.com/NCATS-Tangerine/translator-api-registry/master/semmed/semmed_gene.yaml"
+                        }
+                    }
+                },
+                "response_mapping": {
+                    "positively_regulates": {
+                        "pmid": "positively_regulates.pmid",
+                        "umls": "positively_regulates.umls"
+                    }
+                },
+                "id": "01ba4719c80b6fe911b091a7c05124b64eeece964e09c058ef8f9805daca546b"
+            }
+        }
+    });
+
+    test("test semmed pairInputWithAPIResponse", () => {
+        let tf = new semmed_tf(input);
+        let res = tf.pairInputWithAPIResponse();
+        expect(res["UMLS:C1332823"][0]['umls']).toBe("C1332823");
+        expect(res).toHaveProperty('UMLS:C1332823');
+        expect(res["UMLS:123"]).toBeUndefined();
+    });
+
+    test("test wrapper", () => {
+        let tf = new semmed_tf(input);
+        let res = tf.wrap(input.response[0]);
+        expect(res).toHaveProperty("positively_regulates");
+    });
+
+    test("test json transform", () => {
+        let tf = new semmed_tf(input);
+        let res = tf.jsonTransform(input.response[0]);
+        expect(res).toEqual(input.response[0]);
+    });
+
+    test("add edge info", () => {
+        let tf = new semmed_tf(input);
+        let res = tf.pairInputWithAPIResponse();
+        let rec = res["UMLS:C1332823"][0];
+        rec = tf.wrap(rec);
+        let result = tf.addEdgeInfo("UMLS:C1332823", rec["positively_regulates"][0]);
+        expect(result[0]).toHaveProperty("$association");
+        expect(result[0].$association.api_name).toBe("SEMMED Gene API")
+    });
+
+    test("test main function transform", () => {
+        let tf = new semmed_tf(input);
+        let res = tf.transform();
+        expect(res[0]).toHaveProperty('UMLS');
+        expect(res[0]).toHaveProperty("$association");
+        expect(res[0]).toHaveProperty("$input", "UMLS:C1332823");
+        expect(res.slice(-1)[0]).toHaveProperty("$input", "UMLS:C1332824");
+        expect(res.length).toBeGreaterThan(30);
+    })
+
+})
