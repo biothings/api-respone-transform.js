f47936a60365b7cbcb87be9c3ffaccf56f291f70
tokebe <43009413+tokebe@users.noreply.github.com>
2022-09-14 13:44:20 -0400

    feat: add jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  coverageReporters: ["json-summary"],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+};
