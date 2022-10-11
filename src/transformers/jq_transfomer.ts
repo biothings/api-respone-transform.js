809d73e305a49c4d4f41f109911c0f8c393017dc
Rohan Juneja <rohanj2006@gmail.com>
2022-10-10 18:07:51 -0700

    add comments for transfomers

--- src/transformers/jq_transfomer.ts
+++ src/transformers/jq_transfomer.ts
@@ -3,9 +3,10 @@ import BaseTransformer from "./transformer";
 const jq = require('node-jq');
 
 const filterStrings = {
-  'ebi': '(select(.comments != null) | .comments | .[] | select(.reaction != null) | .reaction.dbReferences | .[]) |= select(.type == "Rhea")',
-  'ctd': '((.[] | select(.PubMedIDs != null) | .PubMedIDs) |= split("|")) | ((.[] | select(.DiseaseID != null) | .DiseaseID) |= (split(":") | last)) | {data: .}',
-  'opentarget': '(.data | .[] | select(.drug.id != null) | select(.drug.id | contains("CHEMBL")) | .drug.id) |= (split("/") | last)',
+  'ebi': '(select(.comments != null) | .comments | .[] | select(.reaction != null) | .reaction.dbReferences | .[]) |= select(.type == "Rhea")', //only take comments where dbReferences type is Rhea
+  'ctd': '((.[] | select(.PubMedIDs != null) | .PubMedIDs) |= split("|")) | ((.[] | select(.DiseaseID != null) | .DiseaseID) |= (split(":") | last)) | {data: .}', //Split pubMedIDs by |, take last element of disease ids after spliting by :
+  'opentarget': '(.data | .[] | select(.drug.id != null) | select(.drug.id | contains("CHEMBL")) | .drug.id) |= (split("/") | last)', // split drug IDs with CHEMBL by /
+  // biolink: two steps - 1. split association object ids & take last (only for CHEMBL, REACT, HGNC), 2. take publications with PMID & for IDs take last element after splitting by :
   'biolink': '[del(.associations), {associations: [select(.associations != null) | .associations | .[] | select(.object.id != null) | (.object.id | split(":") | first) as $pref | .object[$pref] = (if $pref == "HGNC" or $pref == "NCBIGene" or $pref == "REACT" then (.object.id | split(":") | last) else .object.id end) | .publications = [foreach (.publications | .[]? | .id) as $pub ([]; []; if ($pub == null or ($pub | startswith("PMID") | not)) then empty else {id: ($pub | split(":") | last)} end)] ]}] | add'
 }
 
