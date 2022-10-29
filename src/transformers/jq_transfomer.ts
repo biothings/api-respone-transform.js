c37e7c7f616b278c686397c2432013f0a6674dfc
Rohan Juneja <rohanj2006@gmail.com>
2022-10-28 17:36:47 -0700

    make jq strings cleaner

--- src/transformers/jq_transfomer.ts
+++ src/transformers/jq_transfomer.ts
@@ -6,14 +6,9 @@ const jq = require('node-jq');
 const filterStrings = {
   ebi: `
   # only take comments where dbReferences type is Rhea
-  [
-    del(.comments), 
-    {
-      comments: [
-        select(.comments != null) | .comments | .[] | select(.reaction != null) | .reaction.dbReferences = [.reaction.dbReferences | .[] | select(.type == "Rhea")]
-      ]
-    }
-  ] | add
+  .comments = [
+    select(.comments != null) | .comments | .[] | select(.reaction != null) | .reaction.dbReferences = [.reaction.dbReferences | .[] | select(.type == "Rhea")]
+  ]
   `,
   ctd: `
   # Split pubMedIDs by |
@@ -26,25 +21,20 @@ const filterStrings = {
   (.data | .[] | select(.drug.id != null) | select(.drug.id | contains("CHEMBL")) | .drug.id) |= (split("/") | last)
   `,
   biolink: `
-  [
-    del(.associations),
-    {
-      associations: (
-        [
-          # take associations that aren't null
-          (select(.associations != null) | .associations | .[] | 
-          # split association object ids & take last (only for CHEMBL, REACT, HGNC)
-          select(.object.id != null) | (.object.id | split(":") | first) as $pref | .object[$pref] = (if $pref == "HGNC" or $pref == "NCBIGene" or $pref == "REACT" then (.object.id | split(":") | last) else .object.id end) |
-          # take publications with PMID & for IDs take last element after splitting by :
-          .publications = [foreach (.publications | .[]? | .id) as $pub ([]; []; if ($pub == null or ($pub | startswith("PMID") | not)) then empty else {id: ($pub | split(":") | last)} end)])
-          # delete publications if empty array
-          | delifempty(.publications)
-        ] +
-        # include associations with null object ids
-        [select(.associations != null) | .associations | .[] | select(.object.id == null)]
-      )
-    }
-  ] | add
+  .associations = (
+    [
+      # take associations that aren't null
+      (select(.associations != null) | .associations | .[] | 
+      # split association object ids & take last (only for CHEMBL, REACT, HGNC)
+      select(.object.id != null) | (.object.id | split(":") | first) as $pref | .object[$pref] = (if $pref == "HGNC" or $pref == "NCBIGene" or $pref == "REACT" then (.object.id | split(":") | last) else .object.id end) |
+      # take publications with PMID & for IDs take last element after splitting by :
+      .publications = [foreach (.publications | .[]? | .id) as $pub ([]; []; if ($pub == null or ($pub | startswith("PMID") | not)) then empty else {id: ($pub | split(":") | last)} end)])
+      # delete publications if empty array
+      | delifempty(.publications)
+    ] +
+    # include associations with null object ids
+    [select(.associations != null) | .associations | .[] | select(.object.id == null)]
+  )
   # delete association if empty array
   | delifempty(.associations)
   `
