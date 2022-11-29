ffaccc60020030f881481f9509748e286d1b85d6
Rohan Juneja <rohanj2006@gmail.com>
2022-11-28 17:58:28 -0800

    add generate curie function

--- src/jq_utils.ts
+++ src/jq_utils.ts
@@ -9,6 +9,9 @@ def delifempty(k): (if k == [] then del(k) | . else . end);
 
 # sets inputted value to "empty" if empty array
 def remifempty: (if . == [] then empty else . end);
+
+# generates a curie from a type and id
+def generateCurie(idType; id): if (id | type) == "array" then id[0] else id end | split(":") | last | idType + ":" + .;
 `
 
 function generateVariables(variables: JQVariable[]) {
