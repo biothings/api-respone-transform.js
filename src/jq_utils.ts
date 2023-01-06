0ce7b80098077bb4b9e1e5f927af0d59d193fd5a
Rohan Juneja <rohanj2006@gmail.com>
2023-01-05 18:19:53 -0800

    initial list filter function (untested)

--- src/jq_utils.ts
+++ src/jq_utils.ts
@@ -12,6 +12,24 @@ def remifempty: (if . == [] then empty else . end);
 
 # generates a curie from a type and id
 def generateCurie(idType; id): if (id | type) == "array" then id[0] else id end | split(":") | last | idType + ":" + .;
+
+# checks if object meets any filter string
+def any_filter(filter_strs):
+. as $obj | . |= foreach filter_strs as $filter_str (false; . or $obj.[filter_str | split(":") | first] == filter_str | split(":") | last; .);
+
+# checks if object meets all filter strings
+def all_filter(filter_strs):
+. as $obj | . |= foreach filter_strs as $filter_str (false; . or $obj.[filter_str | split(":") | first] == filter_str | split(":") | last; .);
+
+# filters the list that is supplied on all conditions
+def list_filter_all(filter_strs): 
+(filter_str | split(":") | first) as $key | (filter_str | split(":") | last) as $value |
+. |= [.[] | if . | all_filter(filter_strs) then . else empty end];
+
+# filters the list that is supplied on any conditions
+def list_filter_any(filter_strs): 
+(filter_str | split(":") | first) as $key | (filter_str | split(":") | last) as $value |
+. |= [.[] | if . | any_filter(filter_strs) then . else empty end];
 `
 
 function generateVariables(variables: JQVariable[]) {
