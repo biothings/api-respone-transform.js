433bd9a285d756cb252d60a076bf23e0f6f0b9a9
Rohan Juneja <rohanj2006@gmail.com>
2023-03-28 18:34:38 -0700

    remove disease id stuff from ctd

--- src/transformers/ctd_transformer.ts
+++ src/transformers/ctd_transformer.ts
@@ -1,14 +1,12 @@
 import BaseTransformer from "./transformer";
 
 
+
 export default class CTDTransformer extends BaseTransformer {
     wrap(res) {
         res = res.map(item => {
             if (typeof item.PubMedIDs === "string") {
                 item.PubMedIDs = item.PubMedIDs.split('|');
-            }
-            if (typeof item.DiseaseID === "string") {
-                item.DiseaseID = item.DiseaseID.split(':').slice(-1)[0];
             }
             return item;
         });
