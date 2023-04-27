cc8c9001ed1f648e319a05504fa5aca7c51de999
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-27 12:12:24 -0700

    feat: catch sources from trapi sources

--- src/transformers/trapi_transformer.ts
+++ src/transformers/trapi_transformer.ts
@@ -2,58 +2,68 @@ import BaseTransformer from "./transformer";
 import { Record } from "../record";
 
 export default class TRAPITransformer extends BaseTransformer {
-    _getUniqueEdges() {
-        const edges = {};
-        if ('message' in this.data.response && 'results' in this.data.response.message && Array.isArray(this.data.response.message.results)) {
-            this.data.response.message.results.map(item => {
-                edges[item.edge_bindings.e01[0].id] = {
-                    subject: item.node_bindings.n0[0].id,
-                    object: item.node_bindings.n1[0].id
-                }
-            });
-        }
-        return edges;
-    }
-
-    _getEdgeInfo(edgeID) {
-        if ('message' in this.data.response && 'knowledge_graph' in this.data.response.message && 'edges' in this.data.response.message.knowledge_graph) {
-            return this.data.response.message.knowledge_graph.edges[edgeID];
-        }
-        return undefined;
-    }
-
-    _transformIndividualEdge(edge, edgeBinding) {
-        let frozenRecord = {
-          subject: {...this._getSubject(edgeBinding.subject), apiLabel: undefined},
-          object: {
-            original: edgeBinding.object,
-            apiLabel: undefined // could get from API
-          },
-          qualifiers: edge.qualifiers
-            ? Object.fromEntries(
-                edge.qualifiers.map(qualifier => {
-                  return [qualifier.qualifier_type_id, qualifier.qualifier_value];
-                }),
-              )
-            : undefined,
-          association: this.edge.association,
-          qEdge: this.edge.reasoner_edge,
-          mappedResponse: { "edge-attributes": [...edge.attributes] },
+  _getUniqueEdges() {
+    const edges = {};
+    if (
+      "message" in this.data.response &&
+      "results" in this.data.response.message &&
+      Array.isArray(this.data.response.message.results)
+    ) {
+      this.data.response.message.results.map(item => {
+        edges[item.edge_bindings.e01[0].id] = {
+          subject: item.node_bindings.n0[0].id,
+          object: item.node_bindings.n1[0].id,
         };
-
-        if ("subject" in frozenRecord) {
-            return new Record(frozenRecord, this.config, this.edge.association, this.edge.reasoner_edge);
-        }
-
+      });
     }
+    return edges;
+  }
 
-    async transform() {
-        const edgeBindings = this._getUniqueEdges();
-        return Object.keys(edgeBindings).map(edge => {
-            const edgeInfo = this._getEdgeInfo(edge);
-            if (!(typeof edgeInfo === "undefined")) {
-                return this._transformIndividualEdge(edgeInfo, edgeBindings[edge]);
-            }
-        })
+  _getEdgeInfo(edgeID) {
+    if (
+      "message" in this.data.response &&
+      "knowledge_graph" in this.data.response.message &&
+      "edges" in this.data.response.message.knowledge_graph
+    ) {
+      return this.data.response.message.knowledge_graph.edges[edgeID];
     }
+    return undefined;
+  }
+
+  _transformIndividualEdge(edge, edgeBinding) {
+    let frozenRecord = {
+      subject: { ...this._getSubject(edgeBinding.subject), apiLabel: undefined },
+      object: {
+        original: edgeBinding.object,
+        apiLabel: undefined, // could get from API
+      },
+      qualifiers: edge.qualifiers
+        ? Object.fromEntries(
+            edge.qualifiers.map(qualifier => {
+              return [qualifier.qualifier_type_id, qualifier.qualifier_value];
+            }),
+          )
+        : undefined,
+      association: this.edge.association,
+      qEdge: this.edge.reasoner_edge,
+      mappedResponse: {
+        "edge-attributes": [...edge.attributes],
+        trapi_sources: edge.sources
+      },
+    };
+
+    if ("subject" in frozenRecord) {
+      return new Record(frozenRecord, this.config, this.edge.association, this.edge.reasoner_edge);
+    }
+  }
+
+  async transform() {
+    const edgeBindings = this._getUniqueEdges();
+    return Object.keys(edgeBindings).map(edge => {
+      const edgeInfo = this._getEdgeInfo(edge);
+      if (!(typeof edgeInfo === "undefined")) {
+        return this._transformIndividualEdge(edgeInfo, edgeBindings[edge]);
+      }
+    });
+  }
 }
