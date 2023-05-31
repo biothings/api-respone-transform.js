3ff580cdc916e23fc2f1865b1ccc94f0d500a5d4
Rohan Juneja <rohanj2006@gmail.com>
2023-05-30 18:07:12 -0700

    update open target transformer for graphql api

--- src/transformers/opentarget_transformer.ts
+++ src/transformers/opentarget_transformer.ts
@@ -3,12 +3,10 @@ import BaseTransformer from "./transformer";
 
 export default class OpenTargetTransformer extends BaseTransformer {
     wrap(res) {
-        res.data = res.data.map(item => {
-            if ('drug' in item && "id" in item.drug && typeof item.drug.id === "string" && item.drug.id.includes("CHEMBL")) {
-                item.drug.id = item.drug.id.split('/').slice(-1)[0];
-            }
-            return item;
-        });
+        for (const disease of res.data.target.associatedDiseases.rows) {
+            disease.disease.evidences.rows = disease.disease.evidences.rows.filter(r => r.drug != null);
+            disease.disease.evidences.rows = disease.disease.evidences.rows.map(r => ({drug: { ...r.drug, id: r.drug.id.replace("CHEMBL", "") }}))
+        }
         return res;
     }
 }
