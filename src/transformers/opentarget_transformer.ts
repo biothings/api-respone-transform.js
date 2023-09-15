77f6c7fe597de4eb52b74f7a0f73aee972f7a633
Rohan Juneja <rohanj2006@gmail.com>
2023-09-14 17:40:47 -0700

    clean up files

--- src/transformers/opentarget_transformer.ts
+++ src/transformers/opentarget_transformer.ts
@@ -3,13 +3,6 @@ import jq from "node-jq";
 
 export default class OpenTargetTransformer extends BaseTransformer {
     async wrap(res) {
-//         res.data = res.data.map(item => {
-//             if ('drug' in item && "id" in item.drug && typeof item.drug.id === "string" && item.drug.id.includes("CHEMBL")) {
-//                 item.drug.id = item.drug.id.split('/').slice(-1)[0];
-//             }
-//             return item;
-//         });
-//         return res;
         const filterString = '(.data | .[] | select(.drug.id != null) | select(.drug.id | contains("CHEMBL")) | .drug.id) |= (split("/") | last)';
         return await jq.run(filterString, res);
     }
